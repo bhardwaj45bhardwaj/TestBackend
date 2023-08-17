7900b3e0edc5861fb1dc0580659d2b82f0e13aa9
bhardwaj45bhardwaj <sangeeta6.bhardwaj@gmail.com>
2023-08-18 00:54:54 +0530

    added actor search functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,11 +2,5 @@ class MoviesController < ApplicationController
 
 	def index
 		@movies = Movie.get_movie_listing(params[:actor_name])
-		if params[:actor_name].present?
-			@movies.where
-		else
-
-		end
-		@movies 
 	end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,14 +2,25 @@ class Movie < ApplicationRecord
 	has_many :reviews, dependent: :destroy
 
 	def self.get_movie_listing(actor_name)
-		if actor_name.preset? 
-		Movie.find_by_sql(["select m.name, 
-		m.description,
-		m.release_date, 
-		m.actor_name, 
-		avg(r.stars) as average_rating 
-		from movies m join reviews r on m.id = r.movie_id 
-		group by m.id Order By average_rating Desc "])
+		if actor_name.present? 
+			Movie.find_by_sql(["select m.name, 
+			m.description,
+			m.release_date, 
+			m.actor_name, 
+			avg(r.stars) as average_rating 
+			from movies m join reviews r on m.id = r.movie_id
+			where m.actor_name LIKE ? 
+			group by m.id Order By average_rating Desc ", "%#{actor_name}%"])
+		else
+			Movie.find_by_sql(["select m.name, 
+			m.description,
+			m.release_date, 
+			m.actor_name, 
+			avg(r.stars) as average_rating 
+			from movies m join reviews r on m.id = r.movie_id 
+			group by m.id Order By average_rating Desc "])
+		end
 	end
+
 
 end
